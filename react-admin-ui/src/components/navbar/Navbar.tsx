a8dec86ac79b99a0a66c060d0af5cc73d0bc99ac
Hvitrevs <venusof12pm@proton.me>
2023-08-07 18:16:31 -0400

    navbar settings

--- react-admin-ui/src/components/navbar/Navbar.tsx
+++ react-admin-ui/src/components/navbar/Navbar.tsx
@@ -6,15 +6,21 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='logo'>
         <img src='logo1.svg' alt='' className='logo' />
-        <span>Dashboard</span>
+        <span>DASHBOARD</span>
       </div>
       <div className='icons'>
-        <img src='' alt='' className='icon' />
-        <img src='' alt='' className='icon' />
-        <img src='' alt='' className='icon' />
-        <div className='notification'></div>
-        <div className='user'></div>
-        <img src='' alt='' className='icon' />
+        <img src='/search.svg' alt='' className='icon' />
+        <img src='/app.svg' alt='' className='icon' />
+        <img src='/expand.svg' alt='' className='icon' />
+        <div className='notification'>
+          <img src='/notification.svg' alt='' />
+          <span>1</span>
+        </div>
+        <div className='user'>
+          <img src='https://cdna.artstation.com/p/assets/images/images/060/780/462/medium/hvitrev-raiden-shogun.jpg?1679321834' alt='' />
+          <span> John Doe</span>
+        </div>
+        <img src='/settings.svg' alt='' className='icon' />
       </div>
     </div>
   );
