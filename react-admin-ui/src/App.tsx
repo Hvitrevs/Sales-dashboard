0b5301f5ac7eaa66a67283846bfb645a3a59871d
Hvitrevs <venusof12pm@proton.me>
2023-08-07 14:20:33 -0400

    creating layout

--- react-admin-ui/src/App.tsx
+++ react-admin-ui/src/App.tsx
@@ -1,7 +1,10 @@
 import Home from "./pages/home/Home"
-import { createBrowserRouter, RouterProvider, Route, Link, } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
 
 
 function App() {
@@ -9,25 +12,38 @@ function App() {
   const Layout = () => {
     return (
       <div className="main">
-        
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
       </div>
     )
   }
 
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: (
-        <Home />
-      ),
-    },
-    {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/users',
+          element: <Users />
+        },
+        {
+          path: '/products',
+          element: <Products />
+        },
+      ]
     },
   ]);
   
