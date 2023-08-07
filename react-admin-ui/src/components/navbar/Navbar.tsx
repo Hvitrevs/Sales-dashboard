581dd545b63360d2424cf8733877def22157469b
Hvitrevs <venusof12pm@proton.me>
2023-08-07 17:13:04 -0400

    navbar updating

--- react-admin-ui/src/components/navbar/Navbar.tsx
+++ react-admin-ui/src/components/navbar/Navbar.tsx
@@ -6,9 +6,16 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='logo'>
         <img src='logo1.svg' alt='' className='logo' />
-        <span>AdminDashboard</span>
+        <span>Admin Dashboard</span>
       </div>
-      <div className='icons'></div>
+      <div className='icons'>
+        <img src='' alt='' className='icon' />
+        <img src='' alt='' className='icon' />
+        <img src='' alt='' className='icon' />
+        <div className='notification'></div>
+        <div className='user'></div>
+        <img src='' alt='' className='icon' />
+      </div>
     </div>
   );
 };
