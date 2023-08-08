6484f4acd9f3a4e24048f1aeee35dbb5a26466da
Hvitrevs <venusof12pm@proton.me>
2023-08-08 09:58:13 -0400

    sidepanel

--- react-admin-ui/src/components/menu/Menu.tsx
+++ react-admin-ui/src/components/menu/Menu.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import './menu.scss';
+import { menu } from '../../data';
 
 const Menu = () => {
   return (
     <div className='menu'>
+      {menu.map((item) => (
       <div className='item'>
         <span className='title'>MAIN</span>
         <Link to='/' className='listItem'>
@@ -15,19 +17,9 @@ const Menu = () => {
         <span className='listItemTitle'>Profile</span>
         </Link >
       </div>
-      <div className='item'>
-        <span className='title'>MAIN</span>
-        <Link to='/'className='listItem'>
-        <img src='/home.svg' alt='' />
-        <span className='listItemTitle'>Home</span>
-        </Link >
-        <Link to='/'className='listItem'>
-        <img src='/profile.svg' alt='' />
-        <span className='listItemTitle'>Profile</span>
-        </Link >
-      </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
