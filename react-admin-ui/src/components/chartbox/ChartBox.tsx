f55eab0d38640a0fb54827373b2a26737f69e106
Hvitrevs <venusof12pm@proton.me>
2023-08-09 18:19:00 -0400

    chart creating

--- react-admin-ui/src/components/chartbox/ChartBox.tsx
+++ react-admin-ui/src/components/chartbox/ChartBox.tsx
@@ -1,46 +1,51 @@
 import { Link } from 'react-router-dom'
 import './chartBox.scss'
-import {  ResponsiveContainer, RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
-
+import {  ResponsiveContainer, LineChart, Line, } from 'recharts';
 
 
 
 const data = [
     {
-      subject: 'Jan',
-      A: 10,
-      B: 11,
-      fullMark: 11,
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
     },
     {
-      subject: 'Feb',
-      A: 11,
-      B: 12,
-      fullMark: 12,
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
     },
     {
-      subject: 'Mar',
-      A: 11,
-      B: 11,
-      fullMark: 11,
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
     },
     {
-      subject: 'Jun',
-      A: 12,
-      B: 12,
-      fullMark: 12,
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
     },
     {
-      subject: 'Jul',
-      A: 11,
-      B: 11,
-      fullMark: 11,
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
     },
     {
-      subject: 'Aug',
-      A: 12,
-      B: 10,
-      fullMark: 11,
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
     },
   ];
 const ChartBox = () => {
@@ -58,15 +63,11 @@ const ChartBox = () => {
             <div className='chart'>
 
 
-
-                <ResponsiveContainer width="100%" height="100%">
-                    <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
-                        <PolarGrid />
-                        <PolarAngleAxis dataKey="subject" />
-                        <PolarRadiusAxis />
-                        <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#15d6c6" fillOpacity={0.6} />
-                    </RadarChart>
-                    </ResponsiveContainer>
+            <ResponsiveContainer width="100%" height="100%">
+        <LineChart width={300} height={100} data={data}>
+          <Line type="monotone" dataKey="pv" stroke="#3dfbb8" strokeWidth={2} />
+        </LineChart>
+      </ResponsiveContainer>
 
 
             </div>
