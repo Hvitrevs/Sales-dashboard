0b274156fe8df8a103df7dcdf2bc7307b3485b06
Hvitrevs <venusof12pm@proton.me>
2023-08-10 14:37:19 -0400

    added small charts

--- react-admin-ui/src/components/chartbox3/ChartBox3.tsx
+++ react-admin-ui/src/components/chartbox3/ChartBox3.tsx
@@ -1,69 +1,42 @@
 import { Link } from 'react-router-dom'
 import './chartBox3.scss'
-import { BarChart, Bar, ResponsiveContainer } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, } from 'recharts';
 
 const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  { name: 'Prints', value: 44 },
+  { name: 'Art', value: 110 },
+  { name: 'Commissions', value: 15 },
 ];
+
 const ChartBox = () => {
   return (
     <div className='chartBox'>
         <div className='boxInfo'>
             <div className='title'>
                 <img src='/product.svg' alt='' />
-                <span>Monthly Sales</span>
+                <span>Statistics</span>
             </div>
-            <h1>+ 28%</h1>
-            <Link to='/products'> View all sales</Link>
+            <h1>Sales </h1>
+            <Link to='/sales'> View all</Link>
         </div>
         <div className='chartInfo'>
             <div className='chart'>
+
             <ResponsiveContainer width="100%" height="100%">
-              <BarChart width={150} height={40} data={data}>
-                <Bar dataKey="uv" fill="#005c79" />
-              </BarChart>
-            </ResponsiveContainer>
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="value"
+            startAngle={180}
+            endAngle={0}
+            data={data}
+            cx="50%"
+            cy="100%"
+            outerRadius={40}
+            fill="hsla(352, 100.00%, 69.00%, 0.88)"
+            label
+          />
+        </PieChart>
+      </ResponsiveContainer>
 
             </div>
             <div className='texts'>
