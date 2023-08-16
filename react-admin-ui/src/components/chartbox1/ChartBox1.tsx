0963c291004b6cb869be522054bc5f7696deef06
Hvitrevs <venusof12pm@proton.me>
2023-08-16 17:39:44 -0400

    charts fix

--- react-admin-ui/src/components/chartbox1/ChartBox1.tsx
+++ react-admin-ui/src/components/chartbox1/ChartBox1.tsx
@@ -1,90 +1,125 @@
-import { Link } from 'react-router-dom'
-import './chartBox1.scss'
-import { BarChart, Bar, ResponsiveContainer, Tooltip } from 'recharts';
+import { Link } from "react-router-dom";
+import "./chartBox1.scss";
+import { Bar, BarChart, ResponsiveContainer, Tooltip } from "recharts";
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-const ChartBox = () => {
+type Props = {
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+};
+
+const ChartBox1 = (props: Props) => {
   return (
-    <div className='chartBox'>
-        <div className='boxInfo'>
-            <div className='title'>
-                <img src='/product.svg' alt='' />
-                <span>Monthly Sales</span>
-            </div>
-            <h1>+ 28%</h1>
-            <Link to='/products'> View all sales</Link>
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+        <img src='/user.svg' alt='' />
+          <span>{props.title}</span>
         </div>
-        <div className='chartInfo'>
-            <div className='chart'>
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart width={150} height={40} data={data}>
-      <Tooltip
+        <h1>{props.number}</h1>
+        <Link to="/users" style={{ color: props.color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+
+
+          {/* <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
                 contentStyle={{ background: "transparent", border: "none" }}
                 labelStyle={{ display: "none" }}
-                position={{ x: 7, y: 55 }}
-                
+                position={{ x: 10, y: 35 }}
               />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke="hsl(167, 92.60%, 57.50%)"
+                strokeWidth={2}
+              />
+            </LineChart>
+          </ResponsiveContainer> */}
+
+<ResponsiveContainer width="100%" height={100}>
+      <BarChart width={150} height={40} data={props.chartData}>
+        <Tooltip
+          contentStyle={{ background: 'transparent', border: 'none' }}
+          labelStyle={{ display: 'none' }}
+          position={{ x: 7, y: -35 }}
+        />
         <defs>
           <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
             <stop offset="0%" stopColor="hsl(270, 100%, 59%)" />
-            <stop offset="100%" stopColor="hsl(200, 100%, 59%)" />
+            <stop offset="100%" stopColor="hsla(200, 100.00%, 59.00%, 0.00)" />
           </linearGradient>
         </defs>
-        <Bar dataKey="uv" fill="url(#gradient)" />
+        <Bar
+          dataKey={props.dataKey}
+          fill="url(#gradient)"
+          shape={({ x, y, width, height }) => (
+            <rect
+              x={x}
+              y={y}
+              width={width}
+              height={height}
+              rx={7} // You can adjust the corner radius as needed
+              ry={7} // You can adjust the corner radius as needed
+              fill="url(#gradient)"
+            />
+          )}
+          activeShape={({ x, y, width, height }) => (
+            <rect
+              x={x}
+              y={y}
+              width={width}
+              height={height}
+              rx={5}
+              ry={5}
+              fill="url(#gradient)"
+            />
+          )}
+        />
       </BarChart>
     </ResponsiveContainer>
 
-            </div>
-            <div className='texts'>
-                <span className='percentage'></span>
-                <span className='duration'>This month</span>
-            </div>
+          
         </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration">This month</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChartBox
+export default ChartBox1;
+
+
+{/* <ResponsiveContainer width="100%" height="100%">
+<BarChart width={150} height={40} data={data}>
+<Tooltip
+          contentStyle={{ background: "transparent", border: "none" }}
+          labelStyle={{ display: "none" }}
+          position={{ x: 7, y: 55 }}
+          
+        />
+  <defs>
+    <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="0%" stopColor="hsl(270, 100%, 59%)" />
+      <stop offset="100%" stopColor="hsl(200, 100%, 59%)" />
+    </linearGradient>
+  </defs>
+  <Bar dataKey="uv" fill="url(#gradient)" />
+</BarChart>
+</ResponsiveContainer> */}
