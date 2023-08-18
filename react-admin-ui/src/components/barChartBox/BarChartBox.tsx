5ae5f72d9b1f717b10fc5cc09b3f34cef78dd9e6
Hvitrevs <venusof12pm@proton.me>
2023-08-18 16:58:41 -0400

    charts for lower part of screen

--- react-admin-ui/src/components/barChartBox/BarChartBox.tsx
+++ react-admin-ui/src/components/barChartBox/BarChartBox.tsx
@@ -16,15 +16,42 @@ const BarChartBox = (props: Props) => {
     <div className="barChartBox">
         <h1>{props.title}</h1>
         <div className="chart">
-        <ResponsiveContainer width="100%" height="100%">
-        <BarChart width={150} height={40} data={props.chartData}>
-            <Tooltip 
-            contentStyle={{background: "transparent", borderRadius: "5px"}}
-            labelStyle={{display: "none"}}
+        <ResponsiveContainer width="100%" height={150}>
+            <BarChart width={150} height={40} data={props.chartData}>
+            <Tooltip
+                contentStyle={{
+                    background: "hsla(186, 70.10%, 13.10%, 0.42)",
+                    border: "none",
+                    color: "hsl(188, 100.00%, 59.00%)"
+                  
+                  }}
+                labelStyle={{ display: "none", background:"transparent" }}
+                position={{ x: 7, y: -35 }}
+                cursor={{fill:"none"}}
             />
-          <Bar dataKey={props.dataKey} fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
+            <defs>
+              <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="hsl(270, 100%, 59%)" />
+                <stop offset="100%" stopColor="hsl(166, 100.00%, 59.00%)" />
+              </linearGradient>
+            </defs>
+            <Bar
+          dataKey={props.dataKey}
+          fill="hsl(192, 100.00%, 59.00%)"
+          shape={({ x, y, width, height }) => (
+            <rect
+              x={x}
+              y={y}
+              width={width}
+              height={height}
+              rx={20} // You can adjust the corner radius as needed
+              ry={20} 
+              fill="url(#gradient)"
+            />
+          )}
+        />
+          </BarChart>
+          </ResponsiveContainer>
         </div>
     </div>
   )
--- react-admin-ui/src/components/chartbox1/ChartBox1.tsx
+++ react-admin-ui/src/components/chartbox1/ChartBox1.tsx
@@ -40,6 +40,7 @@ const ChartBox1 = (props: Props) => {
                   }}
                 labelStyle={{ display: "none", background:"transparent" }}
                 position={{ x: 7, y: -35 }}
+                cursor={{fill:"none"}}
             />
             <defs>
               <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
