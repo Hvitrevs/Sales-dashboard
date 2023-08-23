a631f94696d79aa94fb30536ea629d4e7f1f33d2
Hvitrevs <venusof12pm@proton.me>
2023-08-23 17:25:37 -0400

    users page styling

--- react-admin-ui/src/components/dataTable/DataTable.tsx
+++ react-admin-ui/src/components/dataTable/DataTable.tsx
@@ -1,9 +1,29 @@
+import { DataGrid } from "@mui/x-data-grid"
 import "./dataTable.scss"
+
+
 
 const DataTable = () => {
   return (
     <div className="dataTable">
-        <h1></h1>
+        <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+            pagination: {
+                paginationModel: {
+                    pageSize: 5,
+                },
+            },
+        }}
+        slotProps={{
+            toolbar:{
+                showQuickFilter:true,
+            }
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick />
     </div>
   )
 }
