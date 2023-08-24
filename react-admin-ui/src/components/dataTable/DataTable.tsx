4435050155f49573183c3dd4c07542f84d121268
Hvitrevs <venusof12pm@proton.me>
2023-08-24 17:59:08 -0400

    code cleanup

--- react-admin-ui/src/components/dataTable/DataTable.tsx
+++ react-admin-ui/src/components/dataTable/DataTable.tsx
@@ -18,53 +18,53 @@ const DataTable = (props:Props) => {
 
 
 
-    const actionColumn:GridColDef ={
+    const actionColumn: GridColDef = {
         field: "action",
         headerName: "Action",
-        width:200,
-        renderCell:(params) => {
-            return(
-                <div className="action">
-                    <Link to={`/${props.slug}/${params.row.id}`}>
-                    <img src="/view.svg" alt="" />
-                    </Link>
-                    <div className="delete" onClick={() => handleDelete(params.row.id)}>
-                        <img src="/delete.svg" alt="" />
-                    </div>
-                </div>
-            )
-        }
+        width: 200,
+        renderCell: (params) => {
+          return (
+            <div className="action">
+              <Link to={`/${props.slug}/${params.row.id}`}>
+                <img src="/view.svg" alt="" />
+              </Link>
+              <div className="delete" onClick={() => handleDelete(params.row.id)}>
+                <img src="/delete.svg" alt="" />
+              </div>
+            </div>
+          );
+        },
+      };
 
-    }
-
-  return (
-    <div className="dataTable">
-        <DataGrid
-        className="dataGrid"
-        rows={props.rows}
-        columns={[... props.columns, actionColumn]}
-        initialState={{
-            pagination: {
+      return (
+        <div className="dataTable">
+          <DataGrid
+            className="dataGrid"
+            rows={props.rows}
+            columns={[...props.columns, actionColumn]}
+            initialState={{
+              pagination: {
                 paginationModel: {
-                    pageSize: 9,
+                  pageSize: 10,
                 },
-            },
-        }}
-        slots={{toolbar:GridToolbar}}
-        slotProps={{
-            toolbar:{
-                showQuickFilter:true,
-                quickFilterProps:{debounceMs: 500},
-            }
-        }}
-        pageSizeOptions={[9]}
-        checkboxSelection
-        disableColumnFilter
-        disableDensitySelector
-        disableColumnSelector
-        disableRowSelectionOnClick />
-    </div>
-  )
-}
-
-export default DataTable;
+              },
+            }}
+            slots={{ toolbar: GridToolbar }}
+            slotProps={{
+              toolbar: {
+                showQuickFilter: true,
+                quickFilterProps: { debounceMs: 500 },
+              },
+            }}
+            pageSizeOptions={[5]}
+            checkboxSelection
+            disableRowSelectionOnClick
+            disableColumnFilter
+            disableDensitySelector
+            disableColumnSelector
+          />
+        </div>
+      );
+    };
+    
+    export default DataTable;
