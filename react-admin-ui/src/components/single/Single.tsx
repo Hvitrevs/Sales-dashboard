e18384cfa3999f8d1cc2e86d12d932e9957f3e65
Hvitrevs <venusof12pm@proton.me>
2023-08-25 17:17:28 -0400

    entering prop data to chart

--- react-admin-ui/src/components/single/Single.tsx
+++ react-admin-ui/src/components/single/Single.tsx
@@ -1,46 +1,18 @@
 import { Bar, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 import "./single.scss"
 
-const data = [
-    {
-      name: 'Page A',
-      uv: 590,
-      pv: 800,
-      amt: 1400,
-    },
-    {
-      name: 'Page B',
-      uv: 868,
-      pv: 967,
-      amt: 1506,
-    },
-    {
-      name: 'Page C',
-      uv: 1397,
-      pv: 1098,
-      amt: 989,
-    },
-    {
-      name: 'Page D',
-      uv: 1480,
-      pv: 1200,
-      amt: 1228,
-    },
-    {
-      name: 'Page E',
-      uv: 1520,
-      pv: 1108,
-      amt: 1100,
-    },
-    {
-      name: 'Page F',
-      uv: 1400,
-      pv: 680,
-      amt: 1700,
-    },
-  ];
+type Props ={
+    id: number;
+    img?: string;
+    title: string;
+    info: object;
+    chart: {
+        dataKeys: { name: string; color: string};
+        data: object[];
+    };
+}
 
-const Single = () => {
+const Single = (props: Props) => {
   return (
     <div className="single">
         <div className="view">
