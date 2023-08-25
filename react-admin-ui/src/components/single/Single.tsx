04da77400bf4db656b4589f0ed750973bfc72e02
Hvitrevs <venusof12pm@proton.me>
2023-08-25 11:05:49 -0400

    building placeholder

--- react-admin-ui/src/components/single/Single.tsx
+++ react-admin-ui/src/components/single/Single.tsx
@@ -1,6 +1,45 @@
 import { Bar, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 import "./single.scss"
 
+const data = [
+    {
+      name: 'Page A',
+      uv: 590,
+      pv: 800,
+      amt: 1400,
+    },
+    {
+      name: 'Page B',
+      uv: 868,
+      pv: 967,
+      amt: 1506,
+    },
+    {
+      name: 'Page C',
+      uv: 1397,
+      pv: 1098,
+      amt: 989,
+    },
+    {
+      name: 'Page D',
+      uv: 1480,
+      pv: 1200,
+      amt: 1228,
+    },
+    {
+      name: 'Page E',
+      uv: 1520,
+      pv: 1108,
+      amt: 1100,
+    },
+    {
+      name: 'Page F',
+      uv: 1400,
+      pv: 680,
+      amt: 1700,
+    },
+  ];
+
 const Single = () => {
   return (
     <div className="single">
@@ -48,6 +87,15 @@ const Single = () => {
 
         </div>
     <div className="activities">
+        <h2> Purchase History </h2>
+        <ul>
+            <li>
+                <div>
+                    <p> John Doe purchased Playstation 5 Digital Edition </p>
+                    <time> 3 days ago </time>
+                </div>
+            </li>
+        </ul>
 
     </div>
         
