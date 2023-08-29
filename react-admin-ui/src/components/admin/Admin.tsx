371ef6acc29a63ee94fcf5acef767a80e8adb337
Hvitrevs <venusof12pm@proton.me>
2023-08-29 16:29:13 -0400

    admin page fix 1/1

--- react-admin-ui/src/components/admin/Admin.tsx
+++ react-admin-ui/src/components/admin/Admin.tsx
@@ -1,84 +1,84 @@
 import { Area, Bar, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 import "./admin.scss"
 
-// const data = {
-//     info: {
-//       username: " Admin",
-//       fullname: " Admin",
-//       email: " corpoemail@organization.com",
-//       phone: " 123 456 789",
-//       status: " verified",
-//     },
-//     chart: {
-//       dataKeys: [
-//         { name: "visits", color: "#70eeff" },
-//         { name: "clicks", color: "#47fa95" },
-//       ],
-//       data: [
-//         {
-//           name: "Sun",
-//           visits: 4000,
-//           clicks: 2400,
-//         },
-//         {
-//           name: "Mon",
-//           visits: 3000,
-//           clicks: 1398,
-//         },
-//         {
-//           name: "Tue",
-//           visits: 2000,
-//           clicks: 3800,
-//         },
-//         {
-//           name: "Wed",
-//           visits: 2780,
-//           clicks: 3908,
-//         },
-//         {
-//           name: "Thu",
-//           visits: 1890,
-//           clicks: 4800,
-//         },
-//         {
-//           name: "Fri",
-//           visits: 2390,
-//           clicks: 3800,
-//         },
-//         {
-//           name: "Sat",
-//           visits: 3490,
-//           clicks: 4300,
-//         },
-//       ],
-//     },
-//     activities: [
-//       {
-//         text: "John Doe purchased Playstation 5 Digital Edition",
-//         time: "3 day ago",
-//       },
-//       {
-//         text: "Catherin Amber added 3 items into their wishlist",
-//         time: "1 week ago",
-//       },
-//       {
-//         text: "Mike Duke purchased Sony Bravia KD-32w800",
-//         time: "2 weeks ago",
-//       },
-//       {
-//         text: "John Winchester reviewed a product",
-//         time: "1 month ago",
-//       },
-//       {
-//         text: "Melissa Hermitage added 1 items into their wishlist",
-//         time: "1 month ago",
-//       },
-//       {
-//         text: "Admin reviewed a feedback left on item #3394-5988-77795-32",
-//         time: "2 months ago",
-//       },
-//     ],
-//   };
+const data = {
+    info: {
+      username: " Admin",
+      fullname: " Admin",
+      email: " corpoemail@organization.com",
+      phone: " 123 456 789",
+      status: " verified",
+    },
+    chart: {
+      dataKeys: [
+        { name: "visits", color: "#70eeff" },
+        { name: "clicks", color: "#47fa95" },
+      ],
+      data: [
+        {
+          name: "Sun",
+          visits: 4000,
+          clicks: 2400,
+        },
+        {
+          name: "Mon",
+          visits: 3000,
+          clicks: 1398,
+        },
+        {
+          name: "Tue",
+          visits: 2000,
+          clicks: 3800,
+        },
+        {
+          name: "Wed",
+          visits: 2780,
+          clicks: 3908,
+        },
+        {
+          name: "Thu",
+          visits: 1890,
+          clicks: 4800,
+        },
+        {
+          name: "Fri",
+          visits: 2390,
+          clicks: 3800,
+        },
+        {
+          name: "Sat",
+          visits: 3490,
+          clicks: 4300,
+        },
+      ],
+    },
+    activities: [
+      {
+        text: "John Doe purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Catherin Amber added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Mike Duke purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "John Winchester reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Melissa Hermitage added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Admin reviewed a feedback left on item #3394-5988-77795-32",
+        time: "2 months ago",
+      },
+    ],
+  };
 
 const Admin = () => {
   return (
@@ -101,7 +101,7 @@ const Admin = () => {
             <hr />
                 <div className="chart">
 
-                    {/* <ResponsiveContainer >
+                    <ResponsiveContainer >
                         <ComposedChart
                             width={100}
                             height={100}
@@ -122,7 +122,7 @@ const Admin = () => {
         </defs>
                     
                         <XAxis 
-                        dataKey="name" 
+                        dataKey="clicks" 
                         scale="band" />
 
                         <YAxis />
@@ -142,15 +142,15 @@ const Admin = () => {
                        
                             <Area
                                 type="monotone"
-                                dataKey={name}
-                                stroke={dataKey.color}
-                                fill="url(#areaGradient)"
+                                dataKey="visits"
+                                stroke="#4ae4ff"
+                                fill="url(#areaGradient2)"
                             />
                             
                             
                         
                         </ComposedChart>
-                    </ResponsiveContainer> */}
+                    </ResponsiveContainer>
                 </div>
             
         </div>
