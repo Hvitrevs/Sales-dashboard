7497c388b7f702e55bf5392af3bea7eeca66040c
Hvitrevs <venusof12pm@proton.me>
2023-09-01 17:03:52 -0400

    polished up login form

--- react-admin-ui/src/pages/login/Login.tsx
+++ react-admin-ui/src/pages/login/Login.tsx
@@ -1,10 +1,16 @@
-import "./login.scss"
-import React, { useEffect, useState } from "react"
 
-import { useNavigate, Link } from "react-router-dom"
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import "./login.scss";
+import React, {  useState } from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import axios from "axios";
+
+import { useNavigate, Link } from "react-router-dom";
+import { Google } from "@mui/icons-material";
 
 
 function Login() {
+    
     const history=useNavigate();
 
     const [email,setEmail]=useState('')
@@ -15,7 +21,7 @@ function Login() {
 
         try{
 
-            await axios.post("http://localhost:8000/signup",{
+            await axios.post("http://localhost:8000/login",{
                 email,password
             })
             .then(res=>{
@@ -45,13 +51,25 @@ function Login() {
           <div className="layoutlogin">
 
               <h1>Authorization</h1>
+              <h5>Welcome Back!</h5>
 
-              <form className="formfield" action="POST">
-                  <input className="forms" type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
+              <form className="formfield" action="/">
+                  <input className="forms" type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder=" Enter your email"  />
 
-                  <input className="forms" type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
+                  <input className="forms" type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Enter your password" />
 
-                  <input className="button" type="submit" onClick={submit} />
+                  <Link to="/">
+                  <button type="submit" onClick={() =>alert('Your credentials are invalid. Redirecting to Demo View. Click "OK to continue')}>Login</button></Link>
+
+                  <hr></hr>
+                  <span className="loginwith">Login with </span>
+                  <div className="socialM">
+                    <Google className="soc" onClick={() =>alert('Login was successfull')} />
+                    <FacebookOutlinedIcon className="soc" onClick={() =>alert('Login was successfull')} />
+                    <LinkedInIcon className="soc" onClick={() =>alert('Login was successfull')} />
+
+                    
+                  </div>
 
               </form>
             
