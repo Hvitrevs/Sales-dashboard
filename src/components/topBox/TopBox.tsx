1249013744a9ccf612a5138a8f06b48ac7471f0f
Hvitrevs <venusof12pm@proton.me>
2023-09-02 14:06:35 -0400

    background fix

--- src/components/topBox/TopBox.tsx
+++ src/components/topBox/TopBox.tsx
@@ -5,20 +5,20 @@ const TopBox = () => {
   return (
     <div className="topBox">
       <h1>Top Sales</h1>
-      <div className="list">
-        {topDealUsers.map(user=>(
-          <div className="listItem" key={user.id}>
-            <div className="user">
-              <img src={user.img} alt="" />
-              <div className="userTexts">
-                <span className="username">{user.username}</span>
-                <span className="email">{user.email}</span>
+        <div className="list">
+            {topDealUsers.map(user=>(
+              <div className="listItem" key={user.id}>
+                <div className="user">
+                  <img src={user.img} alt="" />
+                  <div className="userTexts">
+                    <span className="username">{user.username}</span>
+                    <span className="email">{user.email}</span>
+                  </div>
+                </div>
+                <span className="amount">${user.amount}</span>
               </div>
-            </div>
-            <span className="amount">${user.amount}</span>
+            ))}
           </div>
-        ))}
-      </div>
     </div>
   )
 }
